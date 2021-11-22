55bcfcc8c3f83b2de41a3f08d22fe026d7eff802
Zephan Spencer <number45zephan@gmail.com>
2021-11-22 15:47:26 -0500

    Merging mobilenet changes to pre-release

--- benchmarks/mobilenetv2/classifier_hw_defines.h
+++ benchmarks/mobilenetv2/classifier_hw_defines.h
@@ -1,32 +1,32 @@
 //BEGIN GENERATED CODE
 //Cluster: TAIL_AVGPOOL
 //NonCoherentDMA
-#define CLASSIFIER_DMA_Flags 0x10062480
-#define CLASSIFIER_DMA_RdAddr 0x10062481
-#define CLASSIFIER_DMA_WrAddr 0x10062489
-#define CLASSIFIER_DMA_CopyLen 0x10062491
+#define CLASSIFIER_DMA_Flags 0x100cd5c0
+#define CLASSIFIER_DMA_RdAddr 0x100cd5c1
+#define CLASSIFIER_DMA_WrAddr 0x100cd5c9
+#define CLASSIFIER_DMA_CopyLen 0x100cd5d1
 //StreamDMA
-#define CLASSIFIER_STREAM_DMA0_Flags 0x100624c0
-#define CLASSIFIER_STREAM_DMA0_RdAddr 0x100624c4
-#define CLASSIFIER_STREAM_DMA0_WrAddr 0x100624cc
-#define CLASSIFIER_STREAM_DMA0_RdFrameSize 0x100624d4
-#define CLASSIFIER_STREAM_DMA0_NumRdFrames 0x100624d8
-#define CLASSIFIER_STREAM_DMA0_RdFrameBufSize 0x100624d9
-#define CLASSIFIER_STREAM_DMA0_WrFrameSize 0x100624da
-#define CLASSIFIER_STREAM_DMA0_NumWrFrames 0x100624de
-#define CLASSIFIER_STREAM_DMA0_WrFrameBufSize 0x100624df
-#define CLASSIFIER_STREAM_DMA0_Stream 0x100624e0
+#define CLASSIFIER_STREAM_DMA0_Flags 0x100cd600
+#define CLASSIFIER_STREAM_DMA0_RdAddr 0x100cd604
+#define CLASSIFIER_STREAM_DMA0_WrAddr 0x100cd60c
+#define CLASSIFIER_STREAM_DMA0_RdFrameSize 0x100cd614
+#define CLASSIFIER_STREAM_DMA0_NumRdFrames 0x100cd618
+#define CLASSIFIER_STREAM_DMA0_RdFrameBufSize 0x100cd619
+#define CLASSIFIER_STREAM_DMA0_WrFrameSize 0x100cd61a
+#define CLASSIFIER_STREAM_DMA0_NumWrFrames 0x100cd61e
+#define CLASSIFIER_STREAM_DMA0_WrFrameBufSize 0x100cd61f
+#define CLASSIFIER_STREAM_DMA0_Stream 0x100cd620
 //Accelerator: CLASSIFIER_TOP
-#define CLASSIFIER_TOP 0x10062500
+#define CLASSIFIER_TOP 0x100cd7c0
 //Accelerator: CLASSIFIER_LINEAR
-#define CLASSIFIER_LINEAR 0x10062580
-#define classifier_LinearFeats 0x100625c0
-#define classifier_LinearWeights 0x10062b00
-#define classifier_PWBias 0x1019b340
-#define classifier_PWIMultBias 0x1019b740
-#define classifier_PWNShiftBias 0x1019bb40
-#define classifier_PWIMultOut 0x1019bf40
-#define classifier_PWNShiftOut 0x1019c340
-#define classifier_PWWeightZP 0x1019c740
-#define classifier_LinearSum 0x1019cb40
+#define CLASSIFIER_LINEAR 0x100cd6c0
+#define classifier_LinearFeats 0x100cd700
+#define classifier_LinearWeights 0x100cdc40
+#define classifier_PWBias 0x10206480
+#define classifier_PWIMultBias 0x10206880
+#define classifier_PWNShiftBias 0x10206c80
+#define classifier_PWIMultOut 0x10207080
+#define classifier_PWNShiftOut 0x10207480
+#define classifier_PWWeightZP 0x10207880
+#define classifier_LinearSum 0x10207c80
 //END GENERATED CODE
--- benchmarks/mobilenetv2/tail_hw_defines.h
+++ benchmarks/mobilenetv2/tail_hw_defines.h
@@ -1,37 +1,37 @@
 //BEGIN GENERATED CODE
 //Cluster: BODY_PWCONV1
 //NonCoherentDMA
-#define TAIL_DMA_Flags 0x1003a4c0
-#define TAIL_DMA_RdAddr 0x1003a4c1
-#define TAIL_DMA_WrAddr 0x1003a4c9
-#define TAIL_DMA_CopyLen 0x1003a4d1
+#define TAIL_DMA_Flags 0x10078580
+#define TAIL_DMA_RdAddr 0x10078581
+#define TAIL_DMA_WrAddr 0x10078589
+#define TAIL_DMA_CopyLen 0x10078591
 //StreamDMA
-#define TAIL_STREAM_DMA0_Flags 0x1003a500
-#define TAIL_STREAM_DMA0_RdAddr 0x1003a504
-#define TAIL_STREAM_DMA0_WrAddr 0x1003a50c
-#define TAIL_STREAM_DMA0_RdFrameSize 0x1003a514
-#define TAIL_STREAM_DMA0_NumRdFrames 0x1003a518
-#define TAIL_STREAM_DMA0_RdFrameBufSize 0x1003a519
-#define TAIL_STREAM_DMA0_WrFrameSize 0x1003a51a
-#define TAIL_STREAM_DMA0_NumWrFrames 0x1003a51e
-#define TAIL_STREAM_DMA0_WrFrameBufSize 0x1003a51f
-#define TAIL_STREAM_DMA0_Stream 0x1003a520
+#define TAIL_STREAM_DMA0_Flags 0x100785c0
+#define TAIL_STREAM_DMA0_RdAddr 0x100785c4
+#define TAIL_STREAM_DMA0_WrAddr 0x100785cc
+#define TAIL_STREAM_DMA0_RdFrameSize 0x100785d4
+#define TAIL_STREAM_DMA0_NumRdFrames 0x100785d8
+#define TAIL_STREAM_DMA0_RdFrameBufSize 0x100785d9
+#define TAIL_STREAM_DMA0_WrFrameSize 0x100785da
+#define TAIL_STREAM_DMA0_NumWrFrames 0x100785de
+#define TAIL_STREAM_DMA0_WrFrameBufSize 0x100785df
+#define TAIL_STREAM_DMA0_Stream 0x100785e0
 //Accelerator: TAIL_TOP
-#define TAIL_TOP 0x1003a540
+#define TAIL_TOP 0x10078780
 //Accelerator: TAIL_PWCONV
-#define TAIL_PWCONV 0x1003a5c0
-#define tail_PWConvLocalFeats 0x1003a600
-#define tail_PWConvWeights 0x1003a680
-#define tail_PWBias 0x1005d6c0
-#define tail_PWIMultBias 0x1005dc00
-#define tail_PWNShiftBias 0x1005e140
-#define tail_PWIMultOut 0x1005e680
-#define tail_PWNShiftOut 0x1005ebc0
-#define tail_PWWeightZP 0x1005f100
-#define tail_PWConvOut 0x1005f640
+#define TAIL_PWCONV 0x10078800
+#define tail_PWConvLocalFeats 0x10078840
+#define tail_PWConvWeights 0x10078940
+#define tail_PWBias 0x100c3980
+#define tail_PWIMultBias 0x100c3ec0
+#define tail_PWNShiftBias 0x100c4400
+#define tail_PWIMultOut 0x100c4940
+#define tail_PWNShiftOut 0x100c4e80
+#define tail_PWWeightZP 0x100c53c0
+#define tail_PWConvOut 0x100c5900
 //Accelerator: TAIL_RESHAPE
-#define TAIL_RESHAPE 0x1005f680
-#define tail_ReshapeOut 0x1005f6c0
+#define TAIL_RESHAPE 0x100c5940
+#define tail_ReshapeOut 0x100c5980
 //Accelerator: TAIL_AVGPOOL
-#define TAIL_AVGPOOL 0x10062400
+#define TAIL_AVGPOOL 0x100cd540
 //END GENERATED CODE
