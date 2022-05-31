2c366dd65a5002aafd07fba55cbe241155229f04
miladhakimi <miladhakimi33@gmail.com>
2022-05-31 13:42:49 -0700

    Add matdescent benchmark
    Change-Id: I4e696a4676b56b98baf945ee858ceb0b0db34894

---
+++ benchmarks/AD/matd/hw/matd.c
@@ -0,0 +1,47 @@
+#include "../matd_clstr_hw_defines.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <math.h>
+#include <inttypes.h>
+#include <string.h>
+#define N 30
+#define M 30
+
+extern int enzyme_const;
+template<typename Return, typename... T>
+Return __enzyme_autodiff(T...);
+
+extern "C" {
+    void top();
+}
+
+inline double matvec_real(double* mat, double* vec) {
+  double *out = (double*)malloc(sizeof(double)*N);
+// #pragma clang loop unroll(full)
+  for(int i=0; i<N; i++) {
+    out[i] = 0;
+// #pragma clang loop unroll(full)
+    for(int j=0; j<M; j++) {
+        out[i] += mat[i*M+j] * vec[j];
+    }
+  }
+  double sum = 0;
+  for(int i=0; i<N; i++) {
+    sum += out[i] + out[i];
+  }
+  free(out);
+  return sum;
+}
+
+void top() {
+
+    double *Min = (double *) 0x80C00A00;
+    double *Mout = (double *) 0x80C00A80;
+    double *Vin = (double *) 0x80C00B00;
+    double *Vout = (double *) 0x80C00B80;
+    __enzyme_autodiff<double>(matvec_real, Min, Mout, Vin, Vout);
+
+}
