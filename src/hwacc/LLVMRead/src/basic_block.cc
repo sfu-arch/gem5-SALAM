Josh-Slycord/gem5-SALAM-dev

Fixes to debug output to improve readability

src/hwacc/LLVMRead/src/basic_block.cc
@@ -1,11 +1,13 @@
 //------------------------------------------//
 #include "basic_block.hh"
 #include "llvm/IR/CFG.h"
+#include "sim/sim_object.hh"
 //------------------------------------------//
 
 using namespace SALAM;
 
-SALAM::BasicBlock::BasicBlock(uint64_t id) : SALAM::Value(id) {
+SALAM::BasicBlock::BasicBlock(uint64_t id, gem5::SimObject * owner, bool dbg) :
+    SALAM::Value(id, owner, dbg) {
     // if (DTRACE(Trace)) DPRINTF(Runtime, "Trace: %s \n", __PRETTY_FUNCTION__);
     // if (DTRACE(SALAM_Debug)) {
     //     this->dbg = true;
@@ -39,7 +41,7 @@ SALAM::BasicBlock::BasicBlock_Debugger::dumper(SALAM::BasicBlock *bb)
 void
 SALAM::BasicBlock::initialize(llvm::Value * irval, irvmap *vmap, SALAM::valueListTy * valueList) {
     // if (DTRACE(Trace)) DPRINTF(Runtime, "Trace: %s \n", __PRETTY_FUNCTION__);
-    DPRINTF(LLVMParse, "Initialize Values - BasicBlock::initialize\n");
+    if (dbg) DPRINTFS(LLVMParse, owner, "Initialize Values - BasicBlock::initialize\n");
     Value::initialize(irval, vmap);
 	//Parse irval for BasicBlock params
 	llvm::BasicBlock * bb = llvm::dyn_cast<llvm::BasicBlock>(irval);
@@ -52,7 +54,7 @@ SALAM::BasicBlock::initialize(llvm::Value * irval, irvmap *vmap, SALAM::valueLis
         predecessors.push_back(pred);
     }
 
-    DPRINTF(LLVMParse, "Initialize BasicBlocks\n");
+    if (dbg) DPRINTFS(LLVMParse, owner, "Initialize BasicBlocks\n");
 	for (auto inst_iter = bb->begin(); inst_iter != bb->end(); inst_iter++) {
         llvm::Instruction &inst = *inst_iter;
         std::shared_ptr<SALAM::Value> instval = vmap->find(&inst)->second;
@@ -61,6 +63,6 @@ SALAM::BasicBlock::initialize(llvm::Value * irval, irvmap *vmap, SALAM::valueLis
         assert(instruct);
         instructions.push_back(instruct);
         instruct->initialize(&inst, vmap, valueList);
-        DPRINTF(LLVMParse, "Instruction (UID: %d) Initialization Complete\n", instruct->getUID());
+        if (dbg) DPRINTFS(LLVMParse, owner, "Instruction (UID: %d) Initialization Complete\n", instruct->getUID());
     }
 }

sfu-arch/gem5-SALAM

src/hwacc/LLVMRead/src/basic_block.cc
@@ -1,11 +1,13 @@
 //------------------------------------------//
 #include "basic_block.hh"
 #include "llvm/IR/CFG.h"
+#include "sim/sim_object.hh"
 //------------------------------------------//
 
 using namespace SALAM;
 
-SALAM::BasicBlock::BasicBlock(uint64_t id) : SALAM::Value(id) {
+SALAM::BasicBlock::BasicBlock(uint64_t id, gem5::SimObject * owner, bool dbg) :
+    SALAM::Value(id, owner, dbg) {
     // if (DTRACE(Trace)) DPRINTF(Runtime, "Trace: %s \n", __PRETTY_FUNCTION__);
     // if (DTRACE(SALAM_Debug)) {
     //     this->dbg = true;
@@ -39,7 +41,7 @@ SALAM::BasicBlock::BasicBlock_Debugger::dumper(SALAM::BasicBlock *bb)
 void
 SALAM::BasicBlock::initialize(llvm::Value * irval, irvmap *vmap, SALAM::valueListTy * valueList) {
     // if (DTRACE(Trace)) DPRINTF(Runtime, "Trace: %s \n", __PRETTY_FUNCTION__);
-    DPRINTF(LLVMParse, "Initialize Values - BasicBlock::initialize\n");
+    if (dbg) DPRINTFS(LLVMParse, owner, "Initialize Values - BasicBlock::initialize\n");
     Value::initialize(irval, vmap);
 	//Parse irval for BasicBlock params
 	llvm::BasicBlock * bb = llvm::dyn_cast<llvm::BasicBlock>(irval);
@@ -52,7 +54,7 @@ SALAM::BasicBlock::initialize(llvm::Value * irval, irvmap *vmap, SALAM::valueLis
         predecessors.push_back(pred);
     }
 
-    DPRINTF(LLVMParse, "Initialize BasicBlocks\n");
+    if (dbg) DPRINTFS(LLVMParse, owner, "Initialize BasicBlocks\n");
 	for (auto inst_iter = bb->begin(); inst_iter != bb->end(); inst_iter++) {
         llvm::Instruction &inst = *inst_iter;
         std::shared_ptr<SALAM::Value> instval = vmap->find(&inst)->second;
@@ -67,6 +69,6 @@ SALAM::BasicBlock::initialize(llvm::Value * irval, irvmap *vmap, SALAM::valueLis
             continue;
         }
 
-        DPRINTF(LLVMInterface, "Instruction (UID: %d) Initialization Complete\n", instruct->getUID());
+        if (dbg) DPRINTFS(LLVMInterface, owner, "Instruction (UID: %d) Initialization Complete\n", instruct->getUID());
     }
 }