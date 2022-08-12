Josh-Slycord/gem5-SALAM-dev

Updated MobileNetV2 Headers to support the new memory map

benchmarks/mobilenetv2/tail_hw_defines.h
@@ -1,37 +1,39 @@
 //BEGIN GENERATED CODE
-//Cluster: BODY_PWCONV1
+//Cluster: TAIL
 //NonCoherentDMA
-#define TAIL_DMA_Flags 0x1003a4c0
-#define TAIL_DMA_RdAddr 0x1003a4c1
-#define TAIL_DMA_WrAddr 0x1003a4c9
-#define TAIL_DMA_CopyLen 0x1003a4d1
+#define TAIL_DMA_Flags 0x1003a6c0
+#define TAIL_DMA_RdAddr 0x1003a6c1
+#define TAIL_DMA_WrAddr 0x1003a6c9
+#define TAIL_DMA_CopyLen 0x1003a6d1
 //StreamDMA
-#define TAIL_STREAM_DMA0_Flags 0x1003a500
-#define TAIL_STREAM_DMA0_RdAddr 0x1003a504
-#define TAIL_STREAM_DMA0_WrAddr 0x1003a50c
-#define TAIL_STREAM_DMA0_RdFrameSize 0x1003a514
-#define TAIL_STREAM_DMA0_NumRdFrames 0x1003a518
-#define TAIL_STREAM_DMA0_RdFrameBufSize 0x1003a519
-#define TAIL_STREAM_DMA0_WrFrameSize 0x1003a51a
-#define TAIL_STREAM_DMA0_NumWrFrames 0x1003a51e
-#define TAIL_STREAM_DMA0_WrFrameBufSize 0x1003a51f
-#define TAIL_STREAM_DMA0_Stream 0x1003a520
+#define TAIL_STREAM_DMA0_Flags 0x1003a700
+#define TAIL_STREAM_DMA0_RdAddr 0x1003a704
+#define TAIL_STREAM_DMA0_WrAddr 0x1003a70c
+#define TAIL_STREAM_DMA0_RdFrameSize 0x1003a714
+#define TAIL_STREAM_DMA0_NumRdFrames 0x1003a718
+#define TAIL_STREAM_DMA0_RdFrameBufSize 0x1003a719
+#define TAIL_STREAM_DMA0_WrFrameSize 0x1003a71a
+#define TAIL_STREAM_DMA0_NumWrFrames 0x1003a71e
+#define TAIL_STREAM_DMA0_WrFrameBufSize 0x1003a71f
+#define TAIL_STREAM_DMA0_Stream 0x1003a720
+#define TAIL_STREAM_DMA0_Status 0x1003a740
 //Accelerator: TAIL_TOP
-#define TAIL_TOP 0x1003a540
+#define TAIL_TOP 0x1003a780
 //Accelerator: TAIL_PWCONV
-#define TAIL_PWCONV 0x1003a5c0
-#define tail_PWConvLocalFeats 0x1003a600
-#define tail_PWConvWeights 0x1003a680
-#define tail_PWBias 0x1005d6c0
-#define tail_PWIMultBias 0x1005dc00
-#define tail_PWNShiftBias 0x1005e140
-#define tail_PWIMultOut 0x1005e680
-#define tail_PWNShiftOut 0x1005ebc0
-#define tail_PWWeightZP 0x1005f100
-#define tail_PWConvOut 0x1005f640
+#define TAIL_PWCONV 0x1003a800
+#define tail_PWConvLocalFeats 0x1003a840
+#define tail_PWConvWeights 0x1003a8c0
+#define tail_PWBias 0x1005d900
+#define tail_PWIMultBias 0x1005de40
+#define tail_PWNShiftBias 0x1005e380
+#define tail_PWIMultOut 0x1005e8c0
+#define tail_PWNShiftOut 0x1005ee00
+#define tail_PWWeightZP 0x1005f340
+#define tail_PWConvOut 0x1005f880
+#define tail_PWConvOut_Status 0x1005f8c0
 //Accelerator: TAIL_RESHAPE
-#define TAIL_RESHAPE 0x1005f680
-#define tail_ReshapeOut 0x1005f6c0
+#define TAIL_RESHAPE 0x1005f900
+#define tail_ReshapeOut 0x1005f940
 //Accelerator: TAIL_AVGPOOL
-#define TAIL_AVGPOOL 0x10062400
+#define TAIL_AVGPOOL 0x10062680
 //END GENERATED CODE

sfu-arch/gem5-SALAM

benchmarks/mobilenetv2/tail_hw_defines.h
@@ -1,37 +1,38 @@
 //BEGIN GENERATED CODE
-//Cluster: BODY_PWCONV1
+//Cluster: TAIL
 //NonCoherentDMA
-#define TAIL_DMA_Flags 0x10078580
-#define TAIL_DMA_RdAddr 0x10078581
-#define TAIL_DMA_WrAddr 0x10078589
-#define TAIL_DMA_CopyLen 0x10078591
+#define TAIL_DMA_Flags 0x1003a4c0
+#define TAIL_DMA_RdAddr 0x1003a4c1
+#define TAIL_DMA_WrAddr 0x1003a4c9
+#define TAIL_DMA_CopyLen 0x1003a4d1
 //StreamDMA
-#define TAIL_STREAM_DMA0_Flags 0x100785c0
-#define TAIL_STREAM_DMA0_RdAddr 0x100785c4
-#define TAIL_STREAM_DMA0_WrAddr 0x100785cc
-#define TAIL_STREAM_DMA0_RdFrameSize 0x100785d4
-#define TAIL_STREAM_DMA0_NumRdFrames 0x100785d8
-#define TAIL_STREAM_DMA0_RdFrameBufSize 0x100785d9
-#define TAIL_STREAM_DMA0_WrFrameSize 0x100785da
-#define TAIL_STREAM_DMA0_NumWrFrames 0x100785de
-#define TAIL_STREAM_DMA0_WrFrameBufSize 0x100785df
-#define TAIL_STREAM_DMA0_Stream 0x100785e0
+#define TAIL_STREAM_DMA0_Flags 0x1003a500
+#define TAIL_STREAM_DMA0_RdAddr 0x1003a504
+#define TAIL_STREAM_DMA0_WrAddr 0x1003a50c
+#define TAIL_STREAM_DMA0_RdFrameSize 0x1003a514
+#define TAIL_STREAM_DMA0_NumRdFrames 0x1003a518
+#define TAIL_STREAM_DMA0_RdFrameBufSize 0x1003a519
+#define TAIL_STREAM_DMA0_WrFrameSize 0x1003a51a
+#define TAIL_STREAM_DMA0_NumWrFrames 0x1003a51e
+#define TAIL_STREAM_DMA0_WrFrameBufSize 0x1003a51f
+#define TAIL_STREAM_DMA0_Stream 0x1003a520
 //Accelerator: TAIL_TOP
-#define TAIL_TOP 0x10078780
+#define TAIL_TOP 0x1003a780
 //Accelerator: TAIL_PWCONV
-#define TAIL_PWCONV 0x10078800
-#define tail_PWConvLocalFeats 0x10078840
-#define tail_PWConvWeights 0x10078940
-#define tail_PWBias 0x100c3980
-#define tail_PWIMultBias 0x100c3ec0
-#define tail_PWNShiftBias 0x100c4400
-#define tail_PWIMultOut 0x100c4940
-#define tail_PWNShiftOut 0x100c4e80
-#define tail_PWWeightZP 0x100c53c0
-#define tail_PWConvOut 0x100c5900
+#define TAIL_PWCONV 0x1003a800
+#define tail_PWConvLocalFeats 0x1003a840
+#define tail_PWConvWeights 0x1003a8c0
+#define tail_PWBias 0x1005d900
+#define tail_PWIMultBias 0x1005de40
+#define tail_PWNShiftBias 0x1005e380
+#define tail_PWIMultOut 0x1005e8c0
+#define tail_PWNShiftOut 0x1005ee00
+#define tail_PWWeightZP 0x1005f340
+#define tail_PWConvOut 0x1005f880
+#define tail_PWConvOut_Status 0x1005f8c0
 //Accelerator: TAIL_RESHAPE
-#define TAIL_RESHAPE 0x100c5940
-#define tail_ReshapeOut 0x100c5980
+#define TAIL_RESHAPE 0x1005f900
+#define tail_ReshapeOut 0x1005f940
 //Accelerator: TAIL_AVGPOOL
-#define TAIL_AVGPOOL 0x100cd540
+#define TAIL_AVGPOOL 0x10062400
 //END GENERATED CODE